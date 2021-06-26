27257d351685a761405a41ddff8bc7ab18e5f54d
changwhaj <changwha.jeong@gmail.com>
2021-06-26 10:17:35 +0900

    Update discuss-sap.js

--- AWS/SAP/discuss-sap.js
+++ AWS/SAP/discuss-sap.js
@@ -915,7 +915,7 @@
             //if (passwd == "tssadm") {
             if (discuss_id == "") {
                 url = "https://www.examtopics.com/exams/amazon/aws-certified-solutions-architect-professional/view/" 
-                    + (int(question_id)/10 + 1) +
+                    + String(parseInt(question_id)/10 + 1) +
                     "/";
             } else {
                 url = "https://www.examtopics.com/discussions/amazon/view/" 
